f82c7f942ca32d0ea760724097f5f0ccd86caefe
Piotr Krzeminski <git@krzeminski.it>
2025-08-02 21:47:44 +0200

    Convert to mulitplarform

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,22 @@
 plugins {
-    kotlin("jvm") version "2.1.0"
+    kotlin("multiplatform") version "2.1.0"
 }
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    implementation("com.squareup.okio:okio:3.10.2")
-    implementation("com.squareup.okio:okio-fakefilesystem:3.10.2")
-    implementation("io.ktor:ktor-client-core:3.2.3")
-    implementation("io.ktor:ktor-client-cio:3.2.3")
+kotlin {
+    jvm()
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation("com.squareup.okio:okio:3.10.2")
+                implementation("com.squareup.okio:okio-fakefilesystem:3.10.2")
+                implementation("io.ktor:ktor-client-core:3.2.3")
+                implementation("io.ktor:ktor-client-cio:3.2.3")
+            }
+        }
+    }
 }
